f947af5cecc41d9c54542a0dd7396ed4bd04f15d
Bao Zhiyuan <baozhiyuan@idea.edu.cn>
2024-11-16 12:11:59 +0800

    add surround

--- init.lua
+++ init.lua
@@ -37,6 +37,15 @@ require('lazy').setup({
       { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
     },
+  },
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
   }
 })
 
@@ -48,12 +57,29 @@ if vim.g.vscode then
     no_selection = false
   }
   local k = vim.keymap.set
-  k("n", "]d", function() vscode.action("editor.action.marker.next") end, { noremap = true })
-  k("n", "[d", function() vscode.action("editor.action.marker.prev") end, { noremap = true })
-  k("n", "<leader>r", function() vscode.action("editor.action.rename") end, { noremap = true })
-  k("n", "<leader>l", function() vscode.action("codelens.showLensesInCurrentLine") end, { noremap = true })
-  k("n", "<leader>d", function() vscode.action("editor.debug.action.toggleBreakpoint") end, { noremap = true })
+
+  local function nvim_feedkeys(keys)
+    local feedable_keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
+    vim.api.nvim_feedkeys(feedable_keys, "n", false)
+  end
+
+  local function jump_wrap(keys)
+    return function()
+      vscode.call('jumplist.registerJump')
+      nvim_feedkeys(keys)
+    end
+  end
+
+  k('n', ']d', function() vscode.action('editor.action.marker.next') end, { noremap = true })
+  k('n', '[d', function() vscode.action('editor.action.marker.prev') end, { noremap = true })
+  k('n', '<leader>r', function() vscode.action('editor.action.rename') end, { noremap = true })
+  k('n', '<leader>l', function() vscode.action('codelens.showLensesInCurrentLine') end, { noremap = true })
+  k('n', '<leader>d', function() vscode.action('editor.debug.action.toggleBreakpoint') end, { noremap = true })
+  k('n', 'gd', jump_wrap('gd'), { noremap = true })
+  k('n', 's', jump_wrap('s'), { noremap = true })
   k('n', '<S-m>', 'mciw*', { remap = true })
+  k('n', '<c-o>', function() vscode.action('jumplist.jumpBack') end, { noremap = true })
+  k('n', '<c-i>', function() vscode.action('jumplist.jumpForward') end, { noremap = true })
 else
   -- ordinary Neovim
 end
